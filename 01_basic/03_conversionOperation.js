b9055e0e26cf8ac2a930897ed47bd9f4339cf3fd
guptasonu14 <sguptabudha@gmail.com>
2023-12-24 13:34:09 +0530

    conversion of data types

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33";
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueNumber = Number(score);
+console.log(typeof valueNumber);
+console.log(valueNumber);
+
+//"33" => easity convert =>33
+//"33abc" => NaN
+//true => 1,false => 0
+
+let isLoggedIn  = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true;0 => false
+//"" =>false
+//"sonu" => true
+
+
+let someNumber = 69
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
