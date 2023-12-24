a3ff361dc2874b8da04955f3f0acfa592cdb1bc4
guptasonu14 <sguptabudha@gmail.com>
2023-12-24 13:20:09 +0530

    datatype

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";// treat all js code as newer version
+
+//alert("hello");  //we are using nodejs,not browser
+let name = "sonuGupta";
+let age = 20;
+let isLoggedIn = false;
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => abi value assined nhi hui hai
+//symbol => unique
+
+//object
+console.log(typeof null); //output = object
+console.log(typeof undefined); //output = undefined
+
+
+
+
+
