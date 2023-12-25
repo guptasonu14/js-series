097ab23250d97e5eec47d293dce69bb662ec3a58
guptasonu14 <sguptabudha@gmail.com>
2023-12-25 14:29:43 +0530

    learn about stack and heap memory

--- 01_basic/datatypes-summary.js
+++ 01_basic/datatypes-summary.js
@@ -35,3 +35,27 @@ const myFunction = function(){
 console.log(typeof outSideTemp);
 
 
+//+++++++++++++++++++++++++++++++++++++++++++
+
+//Stack , Heap memory
+//Stack is used in primitive datatypes
+//Heap is used in Non primitive datatypes
+
+
+let myYoutubeName = "journey"
+let anotherName = myYoutubeName;
+anotherName = "chai aur code"
+console.log(myYoutubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@googlr.com",
+    upi: "use@upi"
+}
+
+
+let userTwo = userOne;
+
+userTwo.email = "sonu@google.com";
+console.log(userOne.email);
+console.log(userTwo.email);
