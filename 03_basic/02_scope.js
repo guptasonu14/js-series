0abbea1c9219b94e253f8f7e4b2f7b6c30367e2a
guptasonu14 <sguptabudha@gmail.com>
2023-12-27 21:55:45 +0530

    scope

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -8,3 +8,41 @@ if(true){
 console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+function one(){
+    const username = "gupta"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+   // console.log(website); //it will throw error because it is out of scope
+    two()
+}
+one()
+
+if(true){
+    const username = "sonu"
+    if(username === "sonu"){
+        const website = "youtune"
+        console.log(username+website);
+        
+    }
+  //  console.log(website); //error
+}
+//console.log(username) // throw error
+
+
+//+++++++++++++++ interesting ++++++++++
+
+function addone(num){
+ return num+1
+}
+console.log(addone(5))
+
+
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5)
