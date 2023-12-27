65a4d0549bd1157ae3e084e69f1c90bcd74011ad
guptasonu14 <sguptabudha@gmail.com>
2023-12-27 21:12:52 +0530

    discusion on function

--- 03_basic/01_function.js
+++ 03_basic/01_function.js
@@ -28,4 +28,30 @@ function loginUserMessage(username="sam"){
     return `${username} just logged in`
 }
 //console.log(loginUserMessage("sonu"))
-console.log(loginUserMessage("sonu"));
+//console.log(loginUserMessage("sonu"));
+
+function clculateCartPrice(...num1){
+    return num1
+
+}
+//console.log(clculateCartPrice(200,300,400));
+
+const user = {
+    username:"sonu",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user)
+handleObject({
+    username: "sam",
+    price:99
+})
+
+const myNewArray = [200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,400,100,600]));
